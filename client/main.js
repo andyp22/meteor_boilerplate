2bbd840d138eb5263025eeb476469e68135f5598
Andrew Page <andrew.page32@gmail.com>
2016-04-21 16:24:14 -0400

    Change import statement to reflect file name change

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
 import '../imports/ui/stylesheets/bootstrap-settings.json';
-import '../imports/startup/client/handlebars-helpers.js';
+import '../imports/startup/client/template-helpers.js';
 import '../imports/startup/client/routes.js';
 import '../imports/startup/client/index.js';
